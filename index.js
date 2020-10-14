e40a649f767182fca0a4df515b0b42962d092a64
Your Name <you@example.com>
2020-10-14 16:29:45 +0700

    View user

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-var adapter = new FileSync('db.json')
-var db = low(adapter)
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+var db = low(adapter);
+var shortid= require('shortid');
 
 db.defaults({ users: []})
   .write()
@@ -42,7 +43,17 @@ app.get('/users/create',function(req,res){
  res.render('users/create');
 })
 
+app.get('/users/:id',function(req,res){
+  var id= req.params.id;
+  var user= db.get('users').find({id : id}).value();
+  res.render('users/View',{
+    user: user
+  });
+})
+
+
 app.post('/users/create',function(req,res){
+  req.body.id = shortid.generate();
   db.get('users').push(req.body).write();
   res.redirect('/users');
 })
